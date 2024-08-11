3835f9b6e59ff863b596162e02b6b0365ae22404
Nick Rabone <n.rabone72@aol.com>
2024-08-11 05:46:51 +0100

    Light Dark mode fully working.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import MainNav from '@/components/MainNav'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -21,16 +22,23 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <nav className='flex flex-col items-center border-b mb-5 px-5 py-3'>
-          <div className='max-w-6xl w-full'>
-            <MainNav />
-          </div>
-        </nav>
-        <main className='flex flex-col items-center'>
-          <div className='max-w-6xl w-full'>
-            {children}
-          </div>       
-        </main>  
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <nav className='flex flex-col items-center border-b mb-5 px-5 py-3'>
+            <div className='max-w-6xl w-full'>
+              <MainNav />
+            </div>
+          </nav>
+          <main className='flex flex-col items-center'>
+            <div className='max-w-6xl w-full'>
+              {children}
+            </div>       
+          </main>  
+        </ThemeProvider>
       </body>
     </html>
   )
