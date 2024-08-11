c6d0e524aa2f71950bd0a7672581cf5c4fd1719e
Nick Rabone <n.rabone72@aol.com>
2024-08-11 05:59:19 +0100

    Improving the navbar

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -5,11 +5,7 @@ import ToggleMode from './ToggleMode'
 const MainNav = () => {
   return (
     <div className='flex justify-between'>
-      <div className='flex items-center gap-2'>
-        <Link href='/'>Dashboard</Link>
-        <Link href='/tickets'>Tickets</Link>
-        <Link href='/users'>Users</Link>
-      </div>
+      
       <div className='flex items-center gap-2'>
         <Link href='/'>Logout</Link>
         <ToggleMode />
---
+++ components/MainNavLinks.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const MainNavLinks = () => {
+  return (
+    <div className='flex items-center gap-2'>
+      <Link href='/'>Dashboard</Link>
+      <Link href='/tickets'>Tickets</Link>
+      <Link href='/users'>Users</Link>
+    </div>
+  )
+}
+
+export default MainNavLinks
