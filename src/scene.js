3c03fbb3c705a05e2ac6a558c7cbf7181ea060f4
Nimira43 <n.rabone72@aol.com>
2024-11-20 17:55:39 +0000

    Wrote function to update the camera position.

--- src/scene.js
+++ src/scene.js
@@ -54,7 +54,21 @@ export function createScene() {
       cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
       cameraElevation += ((event.clientY - prevMouseY) * 0.5)
       cameraElevation = Math.min(90, Math.max(0, cameraElevation))
+      updateCameraPosition()
     }
+    prevMouseX = event.clientX
+    prevMouseY = event.clientY
+  }
+
+  function updateCameraPosition() {
+    camera.position.x = cameraRadius * Math.sin(cameraAzimuth * Math.PI / 180) * Math.cos(cameraElevation * Math.PI / 180)
+    
+    camera.position.y = cameraRadius * Math.sin(cameraElevation * Math.PI / 180)
+    
+    camera.position.z = cameraRadius * Math.cos(cameraAzimuth * Math.PI / 180) * Math.cos(cameraElevation * Math.PI / 180)
+
+    camera.lookAt(0, 0, 0)
+    camera.updateMatrix()
   }
 
   return {
