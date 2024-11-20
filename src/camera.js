05864c61be7aa258540f3a7ebb466367e6e878a1
Nimira43 <n.rabone72@aol.com>
2024-11-20 18:57:23 +0000

    Setting up camera position further in onMouseMove - left mouse down handles rotation, middle mouse down handles panning and right mouse down handles zoom

--- src/camera.js
+++ src/camera.js
@@ -14,22 +14,52 @@ export function createCamera(gameWindow) {
   let cameraAzimuth = 0
   let cameraElevation = 0
   let isLeftMouseDown = false
-  let isRightMouseDown = false
   let isMiddleMouseDown = false
+  let isRightMouseDown = false
   let prevMouseX = 0
   let prevMouseY = 0
   updateCameraPosition()
 
-  function onMouseDown() {
-    isMouseDown = true
+  function onMouseDown(event) {
+    if (event.button === LEFT_MOUSE_BUTTON) {
+      isLeftMouseDown = true
+    }
+    if (event.button === MIDDLE_MOUSE_BUTTON) {
+      isMiddleMouseDown = true
+    }
+    if (event.button === RIGHT_MOUSE_BUTTON) {
+      isRightMouseDown = true
+    }
   }
 
-  function onMouseUp() {
-    isMouseUp = false
+  function onMouseUp(event) {
+    if (event.button === LEFT_MOUSE_BUTTON) {
+      isLeftMouseDown = false
+    }
+    if (event.button === MIDDLE_MOUSE_BUTTON) {
+      isMiddleMouseDown = false
+    }
+    if (event.button === RIGHT_MOUSE_BUTTON) {
+      isRightMouseDown = false
+    }
   }
 
   function onMouseMove(event) {
-    if (isMouseDown) {
+    if (isLeftMouseDown) {
+      cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
+      cameraElevation += ((event.clientY - prevMouseY) * 0.5)
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation))
+      updateCameraPosition()
+    }
+    
+    if (isMiddleMouseDown) {
+      cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
+      cameraElevation += ((event.clientY - prevMouseY) * 0.5)
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation))
+      updateCameraPosition()
+    }
+    
+    if (isRightMouseDown) {
       cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
       cameraElevation += ((event.clientY - prevMouseY) * 0.5)
       cameraElevation = Math.min(90, Math.max(0, cameraElevation))
