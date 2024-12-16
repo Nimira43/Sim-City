4f180e04ed399ae98e25bea2c9af2f0de86e37ba
Nimira43 <n.rabone72@aol.com>
2024-12-16 23:36:20 +0000

    Created a simple box for the scene

--- src/scene.js
+++ src/scene.js
@@ -12,6 +12,11 @@ export function createScene() {
   renderer.setSize(gameWindow,offsetWidth, gameWindow.offsetHeight)
   gameWindow.appendChild(renderer.domElement)
 
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  const mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+
   function draw() {
     renderer.render(scene, camera)
   }
