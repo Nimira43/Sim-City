3031086fd87c632c8dde23822c6f7de452eb17fa
Nimira43 <n.rabone72@aol.com>
2024-12-17 02:25:47 +0000

    updated onMouseUp and onMouseDown methods to handle new mouse functionality.  With onMouseMove - this'll handle camera rotation using left mouse button and camera zoom using the right mouse button

--- src/camera.js
+++ src/camera.js
@@ -1,40 +1,80 @@
 import * as THREE from 'three'
 
 export function createCamera(gameWindow) {
+  
   const LEFT_MOUSE_BUTTON = 0
-  const RIGHT_MOUSE_BUTTON = 2
   const MIDDLE_MOUSE_BUTTON = 1
+  const RIGHT_MOUSE_BUTTON = 2
+  const MIN_CAMERA_RADIUS = 2
+  const MAX_CAMERA_RADIUS = 10
+
   const camera = new THREE.PerspectiveCamera(
     75, 
     gameWindow.offsetWidth / gameWindow.offsetHeight, 
     0.1, 
     1000
   )
+
   let cameraRadius = 4
   let cameraAzimuth = 0
   let cameraElevation = 0
   let isLeftMouseDown = false
-  let isRightMouseDown = false
   let isMiddleMouseDown = false
+  let isRightMouseDown = false
   let prevMouseX = 0
   let prevMouseY = 0
+  
   updateCameraPosition()
   
   function onMouseDown(event) {
-    isMouseDown = true
+    if (event.button === LEFT_MOUSE_BUTTON) {
+      isLeftMouseDown = true
+    }
+    if (event.button === MIDDLE_MOUSE_BUTTON) {
+      isMiddleMouseDown = true
+    }
+    if (event.button === RIGHT_MOUSE_BUTTON) {
+      isRightMouseDown = true
+    }
   }
 
   function onMouseUp(event) {
-    isMouseDown = false
+    if (event.button === LEFT_MOUSE_BUTTON) {
+      isLeftMouseDown = false
+    }
+    if (event.button === MIDDLE_MOUSE_BUTTON) {
+      isMiddleMouseDown = false
+    }
+    if (event.button === RIGHT_MOUSE_BUTTON) {
+      isRightMouseDown = false
+    }
   }
   
   function onMouseMove(event) {
-    if (isMouseDown) {
+    
+    // Rotation of camera
+    if (isLeftMouseDown) {
       cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
       cameraElevation += ((event.clientY - prevMouseY) * 0.5)
       cameraElevation = Math.min(90, Math.max(0, cameraElevation))
       updateCameraPosition()
     }
+
+    // Panning of the camera
+    if (isMiddleMouseDown) {
+      cameraAzimuth += -((event.clientX - prevMouseX) * 0.5)
+      cameraElevation += ((event.clientY - prevMouseY) * 0.5)
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation))
+      updateCameraPosition()
+    }
+
+    // Zoom of the camera
+    if (isRightMouseDown) {
+      cameraRadius += (event.clientY - prevMouseY) * 0.02
+      cameraRadius = Math.min(MAX_CAMERA_RADIUS, Math.max(MIN_CAMERA_RADIUS, cameraRadius))
+      updateCameraPosition()
+    }
+
     prevMouseX = event.clientX
     prevMouseY = event.clientY
   }
